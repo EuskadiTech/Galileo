0563aea8eae59a3100f4b4db03df64bb72d7b6c6
Naiel <109038805+naielv@users.noreply.github.com>
2024-11-22 13:03:50 +0100

    Fix

--- modules/Personas/models.py
+++ modules/Personas/models.py
@@ -2,5 +2,3 @@ from pysondb import PysonDB
 from utils import USERDATA_DIR
 
 DB_PERSONAS = PysonDB(USERDATA_DIR + "db.personas.axd")
-DB_PERSONAS.add_new_key("SC_lastcomanda", {})
-DB_PERSONAS.add_new_key("SC_Anilla", "Sin Anilla")
